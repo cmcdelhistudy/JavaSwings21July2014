b3463c52eed59900fad82fbe26210df5d428bb41
User <User@User-PC>
2014-07-21 17:32:14 +0530

    Swing Study

---
+++ src/swingstduy/SwingStduy.java
@@ -0,0 +1,18 @@
+package swingstduy;
+
+import javax.swing.JFrame;
+
+public class SwingStduy {
+    
+    public static void main(String[] args) {
+        
+//        MyFrame mf = new MyFrame();
+//        mf.setVisible(true);
+        
+        StudentFrame sf = new StudentFrame();
+       // sf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        sf.setVisible(true);
+        
+    }
+    
+}
